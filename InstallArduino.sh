2ebb5b180cdaeba8d4b9e872ae473e05d5a166e9
Bertus Kruger <krugercreations@gmail.com>
2016-08-04 20:58:54 +1200

    Update InstallArduino.sh

--- InstallArduino.sh
+++ InstallArduino.sh
@@ -3,22 +3,21 @@
 sudo apt-get install -y python-dev python-rpi.gpio
 
 # install Aruino
-cd  ~
-wget https://downloads.arduino.cc/arduino-1.6.10-linuxarm.tar.xz
-tar xf arduino-1.6.10-linuxarm.tar.xz
-rm arduino-1.6.10-linuxarm.tar.xz
+wget https://downloads.arduino.cc/arduino-1.6.10-linuxarm.tar.xz -P ~
+tar xf ~/arduino-1.6.10-linuxarm.tar.xz
+rm ~/arduino-1.6.10-linuxarm.tar.xz
+
+
 
 
 # Install avrdude-rpi
-cd arduino-1.6.10/hardware/tools/avr/bin
-sudo mv avrdude avrdude-original
-
-wget https://raw.githubusercontent.com/Protoneer/avrdude-rpi/master/autoreset
-wget https://raw.githubusercontent.com/Protoneer/avrdude-rpi/master/avrdude-autoreset
-
-sudo ln -s avrdude-autoreset avrdude
+cp autoreset2560 ~/arduino-1.6.10/hardware/tools/avr/bin/
+cp autoreset328 ~/arduino-1.6.10/hardware/tools/avr/bin/
+cp avrdude-autoreset ~/arduino-1.6.10/hardware/tools/avr/bin/
+sudo mv ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude-orriginal
+sudo ln -s ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude-autoreset ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude
 
 
-sudo chmod +X avrdude
-sudo chmod +X autoreset328
-sudo chmod +X autoreset2560
+sudo chmod +X ~/arduino-1.6.10/hardware/tools/avr/bin/avrdude
+sudo chmod +X ~/arduino-1.6.10/hardware/tools/avr/bin/autoreset328
+sudo chmod +X ~/arduino-1.6.10/hardware/tools/avr/bin/autoreset2560
