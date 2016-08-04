3236a316ad7719dad0e737fc2ec268c61ac70811
Bertus Kruger <krugercreations@gmail.com>
2016-08-04 21:09:45 +1200

    Create Setup_RPI3_Serial.sh

---
+++ Setup_RPI3_Serial.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#  chmod +x filename
+
+sudo sh -c "echo 'dtoverlay=pi3-miniuart-bt' >> /boot/config.txt"
+sudo sh -c "echo 'enable_uart=1' >> /boot/config.txt"
+sudo systemctl disable hciuart
+
+sudo sed -i 's/ console=serial0,115200//g' /boot/cmdline.txt
+
+# New os uses Serial0
+# good read - http://spellfoundry.com/2016/05/29/configuring-gpio-serial-port-raspbian-jessie-including-pi-3/
+
+# RPI 3 only
+sudo systemctl stop serial-getty@ttyS0.service
+sudo systemctl disable serial-getty@ttyS0.service
+
+
+
+sudo shutdown -r now
