d6f18061fe8e5a756699c58b7509faaee6d9bc5d
Michael Langeder <michael@langeder.org>
2016-10-03 23:50:50 +0200

    Add files via upload
    Added config.cfg to use variables for Arduino-IDE version, installation path, etc
    Setup_RPI3_Serial.sh will be called by InstallArduino.sh if necessary

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+Changes by Michael Langeder, Austria
+on Oct. 3rd 2016
+under Apache 2.0, January 2004
+
+
+http://langeder.org
+info@langeder.org
+
+
+###########################################################
+
+Initial scripts by Protoneer from his github repository
+
+http://info.protoneer.co.nz/
+http://blog.protoneer.co.nz/
+http://protoneer.co.nz/
+https://github.com/Protoneer/RaspberryPi-Arduino-avrdude-rpi
+
 # RaspberryPi + Arduino + avrdude-rpi
 
 ### InstallArduino.sh
@@ -5,5 +23,7 @@ Installed Arduino 1.6.10 with avrdude-rpi in place. (For programming Atmega328 /
 
 ### Setup_RPI3_Serial.sh
 Sets up the the serial pins to point to /dev/ttyAMA0 like on older RPi's. (Only needed for Raspberry Pi 3's) 
+
+###########################################################
 
 
--- Setup_RPI3_Serial.sh
+++ Setup_RPI3_Serial.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-#  chmod +x filename
+# chmod +x filename
+
+# 'Setup_RPI3_Serial.sh'
+#Changes by Michael Langeder, Austria
+#on Oct. 3rd 2016
+#under Apache 2.0, January 2004
+#
+#
+#http://langeder.org
+#info@langeder.org
+#
+#
+################################################################################
+#
+#Initial scripts by Protoneer from his github repository
+#
+#http://info.protoneer.co.nz/
+#http://blog.protoneer.co.nz/
+#http://protoneer.co.nz/
+#https://github.com/Protoneer/RaspberryPi-Arduino-avrdude-rpi
+#
+################################################################################
+
 
 sudo sh -c "echo 'dtoverlay=pi3-miniuart-bt' >> /boot/config.txt"
 sudo sh -c "echo 'enable_uart=1' >> /boot/config.txt"
@@ -14,6 +36,7 @@ sudo sed -i 's/ console=serial0,115200//g' /boot/cmdline.txt
 sudo systemctl stop serial-getty@ttyS0.service
 sudo systemctl disable serial-getty@ttyS0.service
 
+# rebooting might not be necessary
+# sudo shutdown -r now
 
 
-sudo shutdown -r now
